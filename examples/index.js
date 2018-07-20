beb2ade6f2665ae95d3aaa53e71314b7dfd7448a
Caio Gondim <me@caiogondim.com>
2018-07-19 23:40:07 -0400

    Working with observables

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import withSubscribe from 'with-subscribe'
-// import CityState from '../lib'
+import { Observable, interval } from 'rxjs'
+import $$observable from 'symbol-observable'
+
+class Subscribe extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      last: []
+    }
+  }
+
+  componentDidMount() {
+    this.props.to.forEach((observable, index) => {
+      observable[$$observable]().subscribe({
+        next: val => {
+          let last
+          if (this.state.last.length === 0) {
+            last = new Array(this.props.to.length)
+            last[index] = val
+          } else {
+            last = [ ...this.state.last ].map((item, index_) => {
+              if (index_ === index) {
+                return val
+              }
+              return item
+            })
+          }
+
+          this.setState({
+            last
+          })
+        }
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.children(...this.state.last)}
+      </div>
+    )
+  }
+}
+
+//
+// Models
+//
 
 const Counter = withSubscribe(class {
   constructor() {
@@ -18,34 +65,55 @@ const Counter = withSubscribe(class {
 })
 const counter = new Counter()
 
-class Look extends React.Component {
-  componentDidMount() {
-    this.props.for.forEach(observable => {
-      observable.subscribe(() => this.forceUpdate())
-    })
+//
+// Views
+//
+
+function CounterView() {
+  return (
+    <Subscribe to={[counter]}>
+      {() => (
+        <div>
+          <h1>Counter</h1>
+          <p>state: {counter.count}</p>
+          <button onClick={() => counter.increment()}>Increment +</button><br />
+          <button onClick={() => counter.decrement()}>Decrement -</button><br />
+        </div>
+      )}
+    </Subscribe>
+  )
+}
+
+class TimerView extends React.Component {
+  constructor() {
+    super()
+    this.interval1 = interval(2000)
+    this.interval2 = interval(1000)
   }
 
   render() {
     return (
-      <div>
-        {this.props.children(...this.props.for)}
-      </div>
+      <Subscribe to={[this.interval1, this.interval2]}>
+        {(time1, time2) => {
+          return (
+              <div>
+                <h1>Timer</h1>
+                <p>time1: {time1}</p>
+                <p>time2: {time2}</p>
+              </div>
+            )
+        }}
+      </Subscribe>
     )
   }
 }
 
 function Main() {
   return (
-    <Look for={[counter]}>
-      {(counter) => (
-        <div>
-          <h1>Counter</h1>
-          <p>state: {counter.count}</p>
-          <button onClick={() => counter.increment()}>+</button><br />
-          <button onClick={() => counter.decrement()}>-</button><br />
-        </div>
-      )}
-    </Look>
+    <div>
+      <CounterView />
+      <TimerView />
+    </div>
   )
 }
 
